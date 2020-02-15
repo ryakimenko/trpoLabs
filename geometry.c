d6278ab62c0c105f96c6168723cdd82ab8aab5bf
Roman Yakimenko <romrom2001@mail.ru>
2020-02-15 18:38:11 +0700

    Create file geometry.c and describe structers like Point, Circle and Triangle

---
+++ geometry.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct Point {
+    int x;
+    int y;
+};
+typedef struct Triangle {
+    Point.p1;
+    Point.p2;
+    Point.p3;
+};
+typedef struct Circle {
+    Point.centre;
+    float radius;
+};
+
+int main()
+{
+    
+    return 0;
+}
